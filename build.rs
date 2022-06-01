e325da58f4db249f810c75ec801971b20f2e7733
codie <constantin.diez@gmx.de>
2022-06-01 23:19:44 +0200

    adapt build rs

--- build.rs
+++ build.rs
@@ -16,12 +16,9 @@ extern crate tonic_build;
 // }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let event_service_descriptor_set_path: PathBuf =
-    //     PathBuf::from(env::var("OUT_DIR").unwrap()).join("grpc_monitoring_v1.bin");
     tonic_build::configure()
-        // .file_descriptor_set_path(event_service_descriptor_set_path)
         .build_server(true)
-        // .build_client(true)
+        .build_client(true)
         .compile(&["protobuf/change_events.proto"], &["protobuf"])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 
