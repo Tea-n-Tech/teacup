263286475eeaf0edc9495c9e21aed9e453ecd982
codie <constantin.diez@gmx.de>
2022-06-11 14:17:48 +0200

    clean up

--- src/client.rs
+++ src/client.rs
@@ -12,16 +12,19 @@ struct EventSubmitter {
 
 impl Drop for EventSubmitter {
     fn drop(&mut self) {
-        if let Some(submission_handler) = self.submission_handler.take() {
-            submission_handler.abort();
+        match self.submission_handler {
+            Some(ref mut submission_handler) => {
+                submission_handler.abort();
+            }
+            None => {}
         }
     }
 }
 
 impl EventSubmitter {
-    pub async fn new(channel: Channel) -> Self {
+    pub fn new(client: EventServiceClient<Channel>) -> Self {
         Self {
-            client: EventServiceClient::new(channel),
+            client: client,
             submission_handler: None,
         }
     }
@@ -40,9 +43,9 @@ impl EventSubmitter {
         println!("Got initial state: {:?}", initial_state);
 
         // collect data indefinitely and send data to the channel
-        let collect_handler = tokio::task::spawn(async {
+        self.submission_handler = Some(tokio::task::spawn(async {
             data_collection::collect_events(tx).await;
-        });
+        }));
 
         loop {
             match rx.recv().await {
@@ -64,45 +67,6 @@ impl EventSubmitter {
     }
 }
 
-async fn start_submission(mut client: EventServiceClient<Channel>) {
-    // communication channels between data collection and sending
-    let (tx, mut rx) = mpsc::channel::<data_collection::proto::ChangeEventBatch>(32);
-
-    // collect data indefinitely and send data to the channel
-    let collect_handler = tokio::task::spawn(async {
-        data_collection::collect_events(tx).await;
-    });
-
-    println!("Fetching initial state");
-    let initial_state_result = client.initial_state(tonic::Request::new(())).await;
-    if let Err(err) = initial_state_result {
-        eprintln!("Failed to get initial state: {}", err);
-        // return err;
-    }
-
-    let initial_state = initial_state_result.unwrap().into_inner();
-    println!("Got initial state: {:?}", initial_state);
-    // TODO init data collection from initial state
-
-    loop {
-        match rx.recv().await {
-            Some(event_batch) => {
-                println!("Sending events {:?}", event_batch);
-                let request = tonic::Request::new(event_batch);
-                match client.send_events(request).await {
-                    Ok(response) => {
-                        println!("RESPONSE={:?}", response);
-                    }
-                    Err(e) => {
-                        eprintln!("Error sending events: {:?}", e);
-                    }
-                }
-            }
-            None => {}
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // receive change events from a channel and send them to the
@@ -113,17 +77,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             eprintln!("Failed to connect to the server: {}", e);
             return;
         }
-        let mut client = client_result.unwrap();
+        let client = client_result.unwrap();
 
-        'submit_loop: loop {}
+        let mut submitter = EventSubmitter::new(client);
+        'submit_loop: loop {
+            match submitter.submit_events().await {
+                Ok(_) => {
+                    break 'submit_loop;
+                }
+                Err(e) => {
+                    eprintln!("Error submitting events: {:?}", e);
+                    println!("Waiting 5 seconds before trying again.");
+                    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+                }
+            }
+        }
     });
 
-    match try_join!(collect_handler, send_handler) {
+    match send_handler.await {
         Ok(_) => {}
         Err(e) => {
-            println!("Error waiting for handlers: {:?}", e);
+            eprintln!("Error sending events: {:?}", e);
         }
-    };
+    }
 
     Ok(())
 }
